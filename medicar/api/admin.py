728988fd1a964b1f335ec39304d4087cac02c112
emanuelsdsj <emanuelsdsj@gmail.com>
2020-09-20 15:54:36 -0300

    Remove consultas

--- medicar/api/admin.py
+++ medicar/api/admin.py
@@ -4,4 +4,3 @@ from .models import Especialidade, Medico, Agenda, Consulta
 admin.site.register(Especialidade)
 admin.site.register(Medico)
 admin.site.register(Agenda)
-admin.site.register(Consulta)
